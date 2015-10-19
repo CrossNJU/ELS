2f1cae428a26928a5f33675a37e228e578633ae5
PolarisChen <polaris_chen@163.com>
2015-10-19 09:45:47 +0800

    Add PO package.

--- ELSClient/src/org/cross/elsclient/dataservice/logdataservice/LogDataService.java
+++ ELSClient/src/org/cross/elsclient/dataservice/logdataservice/LogDataService.java
@@ -1,5 +1,14 @@
 package org.cross.elsclient.dataservice.logdataservice;
 
-public class LogDataService {
+import java.util.ArrayList;
 
+import org.cross.elsclient.po.LogPO;
+
+public interface LogDataService {
+	
+	public void insert(LogPO po);
+	
+	public ArrayList<LogPO> find(String startTime, String endTime);
+	
+	public ArrayList<LogPO> show();
 }
