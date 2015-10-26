4e159ba0bd0e4d55de00f23b8b04720f4ec94568
cccccdn <524749923@qq.com>
2015-10-26 09:23:06 +0800

    Update GoodsDataService_Stub

--- ELSClient/src/org/cross/elsclient/dataservice/goodsdataservice/GoodsDataService_Driver.java
+++ ELSClient/src/org/cross/elsclient/dataservice/goodsdataservice/GoodsDataService_Driver.java
@@ -8,12 +8,15 @@ package org.cross.elsclient.dataservice.goodsdataservice;
 import java.rmi.RemoteException;
 
 import org.cross.elsclient.po.GoodsPO;
+import org.cross.elsclient.po.Receipt_OrderPO;
 import org.cross.elsclient.util.City;
 
 public class GoodsDataService_Driver {
 	public void driver(GoodsDataService goodsDataService) throws RemoteException{
 		
 		GoodsPO goods = new GoodsPO(45, 43, City.NANJING);
+		Receipt_OrderPO receipt = new Receipt_OrderPO("R120151023000004", "2015-10-25 10:10:10");
+		goods.setOrderPO(receipt);
 		
 		System.out.println("更新快件信息");
 		goodsDataService.update(goods);
