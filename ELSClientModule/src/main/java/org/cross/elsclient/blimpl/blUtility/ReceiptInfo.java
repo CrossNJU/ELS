421438dd08973894c88aa0182272312b3860aa74
RickChem <1395314348@qq.com>
2015-12-08 09:39:20 +0800

    Modify receiptinfo

--- ELSClientModule/src/main/java/org/cross/elsclient/blimpl/blUtility/ReceiptInfo.java
+++ ELSClientModule/src/main/java/org/cross/elsclient/blimpl/blUtility/ReceiptInfo.java
@@ -1,9 +1,9 @@
 package org.cross.elsclient.blimpl.blUtility;
 
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 
 import org.cross.elsclient.vo.ReceiptVO;
-import org.cross.elsclient.vo.Receipt_OrderVO;
 import org.cross.elscommon.po.ReceiptPO;
 import org.cross.elscommon.util.ReceiptType;
 
@@ -18,9 +18,9 @@ public interface ReceiptInfo {
 	 */
 	public ReceiptVO findByID(String names) throws RemoteException;
 	
-	public Receipt_OrderVO findByMoneyin(String moneyinNum) throws RemoteException;
+	public ArrayList<String> findByMoneyin(String moneyinNum) throws RemoteException;
 	
-	public ReceiptVO findByTimeAndType(ReceiptType type, String start, String end) throws RemoteException;
+	public ArrayList<ReceiptVO> findByTimeAndType(ReceiptType type, String start, String end) throws RemoteException;
 	
 	
 }
