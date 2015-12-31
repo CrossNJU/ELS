942a630b27145e1723804241e8dbe5a2ecab7cda
RickChem <1395314348@qq.com>
2015-12-31 16:32:33 +0800

    Update constanval

--- ELSClientModule/src/main/java/org/cross/elsclient/util/ConstantVal.java
+++ ELSClientModule/src/main/java/org/cross/elsclient/util/ConstantVal.java
@@ -1,6 +1,7 @@
 package org.cross.elsclient.util;
 
 import java.rmi.RemoteException;
+import java.util.ArrayList;
 
 import org.cross.elsclient.blimpl.blUtility.ConstantInfo;
 import org.cross.elsclient.blimpl.blfactoryimpl.BLFactoryImpl;
@@ -11,16 +12,20 @@ import org.cross.elsclient.blservice.constantblservice.ConstantBLService;
 import org.cross.elsclient.blservice.numberblservice.NumberBLService;
 import org.cross.elsclient.network.Datafactory;
 import org.cross.elsclient.vo.ConstantVO;
+import org.cross.elsclient.vo.ReceiptVO;
 import org.cross.elscommon.dataservice.datafactoryservice.DataFactoryService;
 import org.cross.elscommon.dataservice.numberdataservice.NumberDataService;
 import org.cross.elscommon.po.ConstantPO;
 import org.cross.elscommon.po.NumberPO;
+import org.cross.elscommon.po.ReceiptPO;
 import org.cross.elscommon.util.NumberType;
 
 public class ConstantVal {
 	public static ConstantVO CONSTANT;
 	public static ConstantBLService constantbl;
 	public static NumberBLService numberbl;
+	
+	public static ArrayList<ReceiptVO> currentReceipts;
 	
 	public static ConstantBLService getConstant(){
 		DataFactoryService dataFactoryService = new Datafactory();
