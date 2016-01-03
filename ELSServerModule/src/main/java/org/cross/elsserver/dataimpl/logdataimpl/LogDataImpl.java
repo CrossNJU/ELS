5bc8399a033ea9c5bfe5350f1be275dc5ca4ef2d
RickChem <1395314348@qq.com>
2016-01-03 22:59:04 +0800

    Modify Log

--- ELSServerModule/src/main/java/org/cross/elsserver/dataimpl/logdataimpl/LogDataImpl.java
+++ ELSServerModule/src/main/java/org/cross/elsserver/dataimpl/logdataimpl/LogDataImpl.java
@@ -5,9 +5,11 @@ import java.rmi.server.UnicastRemoteObject;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import org.cross.elscommon.dataservice.logdataservice.LogDataService;
 import org.cross.elscommon.po.LogPO;
+import org.cross.elscommon.po.ReceiptPO;
 import org.cross.elscommon.util.CompareTime;
 import org.cross.elscommon.util.ResultMessage;
 import org.cross.elsserver.database.MySQL;
@@ -38,17 +40,26 @@ public class LogDataImpl extends UnicastRemoteObject implements LogDataService {
 	@Override
 	public ArrayList<LogPO> find(String startTime, String endTime) throws RemoteException {
 		ArrayList<LogPO> logs = show();
-		for (int i = 0; i < logs.size(); i++) {
-			boolean con1 = false;
-			boolean con2 = false;
-			if (CompareTime.compare(startTime, logs.get(i).getTime()) == 1)
-				con1 = true;
-			if (CompareTime.compare(logs.get(i).getTime(), endTime) == 1)
-				con2 = true;
-			if (con1 || con2) {
-				logs.remove(i);
+		Iterator<LogPO> ite = logs.iterator();
+		while(ite.hasNext()) {
+			LogPO po = ite.next();
+			if (CompareTime.compare(po.getTime(), startTime) != 1
+					|| CompareTime.compare(endTime, po.getTime()) != 1) {
+//				ReceiptPO po = findByNum(rs.getString("number"));
+				ite.remove();
 			}
 		}
+//		for (int i = 0; i < logs.size(); i++) {
+//			boolean con1 = false;
+//			boolean con2 = false;
+//			if (CompareTime.compare(startTime, logs.get(i).getTime()) == 1)
+//				con1 = true;
+//			if (CompareTime.compare(logs.get(i).getTime(), endTime) == 1)
+//				con2 = true;
+//			if (con1 || con2) {
+//				logs.remove(i);
+//			}
+//		}
 		return logs;
 	}
 
@@ -79,7 +90,7 @@ public class LogDataImpl extends UnicastRemoteObject implements LogDataService {
 
 	public static void main(String[] args) throws RemoteException{
 		LogDataImpl impl = new LogDataImpl();
-		ArrayList<LogPO> show = impl.find("2015-10-1", "2015-10-2");
+		ArrayList<LogPO> show = impl.find("2015-10-4", "2015-12-25");
 		for (int i = 0; i < show.size(); i++) {
 			System.out.println(show.get(i).getNumber() + " " + show.get(i).getOperation()
 					+ " " + show.get(i).getTime());
